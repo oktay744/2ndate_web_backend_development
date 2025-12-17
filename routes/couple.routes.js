25f92bffe177ec0eb5f813eed61a8f38bbf4ca4b
Oktay Altunkaya <colooktay2@gmail.com>
2025-12-17 20:59:20 +0300

    refactoring

--- routes/couple.routes.js
+++ routes/couple.routes.js
@@ -5,9 +5,9 @@ import { verifyToken } from '../middleware/verifyToken.js';
 const router = express.Router();
 
 router.post('/invite', verifyToken, createInvite);
-router.get('/myInvites', verifyToken, getMyInvites);
 router.get('/:inviteKey', getInvite); 
 router.post('/:inviteKey/complete', completeInvite);
 router.get('/:inviteKey/result', getCoupleResult);
+router.get('/myInvites', verifyToken, getMyInvites);
 
 export default router;
